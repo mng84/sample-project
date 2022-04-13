47ec1f2bec116f72021eaf5e41f00a983ff11ef7
martinos <mg@martinosgeorgiou.com>
2022-04-13 09:53:34 +0300

    Small Changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     //Show scroll to top button
     $(".scrollToTop").fadeOut(10);
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 500) {
             $(".scrollToTop").fadeIn(1000);
         } else {
@@ -11,7 +11,7 @@ $(document).ready(function () {
     });
 
     //Animate to the top of the page
-    $(".scrollToTop").click(function () {
+    $(".scrollToTop").click(function() {
         $("html,body").animate({
             scrollTop: 0
         }, 1000)
@@ -71,62 +71,9 @@ $(document).ready(function () {
 
 //end of jquery
 
-
 const mobileButton = document.querySelector(".c-burger-container")
 const navMenu = document.querySelector(".c-main-navigation-container")
 mobileButton.addEventListener("click", (e) => {
     e.currentTarget.classList.toggle("c-active")
     navMenu.classList.toggle("c-active")
 })
-
-
-// let tabButtons = document.querySelectorAll(".about-us-tab-title")
-// let tabContent = document.querySelectorAll(".c-about-us-timline-title-container")
-
-// tabButtons.forEach((button) => {
-
-//     button.addEventListener("click", () => {
-
-//         let tabTarget = button.dataset.open
-
-//         tabContent.forEach((tab) => {
-
-//             if (tabTarget === tab.id) {
-//                 tab.classList.add("is-selected")
-//             } else {
-//                 tab.classList.remove("is-selected")
-//             }
-
-//         })
-
-//     })
-
-// })
-
-// //Accordion
-
-// let allAccordions = document.querySelectorAll(".c-accordion-container");
-// allAccordions = Array.from(allAccordions);
-
-// allAccordions.forEach((accordion) => {
-//     accordion.addEventListener("click", (e) => {
-//         //        let current = document.querySelector(".c-accordion-active")
-//         e.currentTarget.classList.toggle("c-accordion-active");
-//         //        current.classList.remove("c-accordion-active")
-//         //        accordion.classList.toggle("c-accordion-active")
-//     });
-// });
-
-// //Accordion title add color to title if it is current
-// let accordionTitle = document.querySelectorAll(".c-accordion-title-container");
-// accordionTitle = Array.from(accordionTitle);
-
-
-// accordionTitle.forEach((e) => {
-//     e.addEventListener("click", (v) => {
-//         accordionTitle.forEach((x) => {
-//             x.style.color = "#303030";
-//             v.currentTarget.style.color = "#929497";
-//         })
-//     })
-// })
